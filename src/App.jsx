8bed4a3274f1ab741ef7d628cf1d55705dffa702
Dennydaffa <dennydaffarizaldy16@gmail.com>
2024-02-21 22:33:17 +0700

    Fix Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+// IMPORT COMPNENTS
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
-import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
+// IMPORT PAGES
+import Home from "./Pages/Home";
+
 function App() {
   const [count, setCount] = useState(0);
 
