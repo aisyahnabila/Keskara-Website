d8e71e657db0ffcb36d33b3d7ec9473949fd4aa7
Dennydaffa <dennydaffarizaldy16@gmail.com>
2024-02-27 11:17:53 +0700

    Table Fix?

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer class="main-footer">
-      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+      <strong>Copyright &copy; 2024 <a href="https://adminlte.io">KESKERA</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
