8975710d0af99d8ddd0de0dc82e0aa053b96fa30
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-02-28 14:09:25 +0700

    Update dashboard: change color and icon in dashboard

--- src/Pages/Beranda.jsx
+++ src/Pages/Beranda.jsx
@@ -45,41 +45,7 @@ const Beranda = () => {
                     <p>Total Anggota Satuan</p>
                   </div>
                   <div class="icon">
-                    <i class="ion ion-bag" />
-                  </div>
-                  <a href="#" class="small-box-footer">
-                    Detail Info <i class="fas fa-arrow-circle-right" />
-                  </a>
-                </div>
-              </div>
-              {/* ./col */}
-              <div class="col-lg-3 col-6">
-                {/* small box */}
-                <div class="small-box bg-success">
-                  <div class="inner">
-                    <h3>
-                      53<sup style={{ fontSize: 20 }}>%</sup>
-                    </h3>
-                    <p>Anggota Overweight</p>
-                  </div>
-                  <div class="icon">
-                    <i class="ion ion-stats-bars" />
-                  </div>
-                  <a href="#" class="small-box-footer">
-                    Detail Info <i class="fas fa-arrow-circle-right" />
-                  </a>
-                </div>
-              </div>
-              {/* ./col */}
-              <div class="col-lg-3 col-6">
-                {/* small box */}
-                <div class="small-box bg-warning">
-                  <div class="inner">
-                    <h3>44</h3>
-                    <p>Anggota Ideal</p>
-                  </div>
-                  <div class="icon">
-                    <i class="ion ion-person-add" />
+                    <i class=" ion ion-ios-people" />
                   </div>
                   <a href="#" class="small-box-footer">
                     Detail Info <i class="fas fa-arrow-circle-right" />
@@ -91,11 +57,45 @@ const Beranda = () => {
                 {/* small box */}
                 <div class="small-box bg-danger">
                   <div class="inner">
+                    <h3>
+                      53<sup style={{ fontSize: 20 }}>%</sup>
+                    </h3>
+                    <p>Anggota Overweight</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-ios-body" />
+                  </div>
+                  <a href="#" class="small-box-footer">
+                    Detail Info <i class="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+              {/* ./col */}
+              <div class="col-lg-3 col-6">
+                {/* small box */}
+                <div class="small-box bg-success">
+                  <div class="inner">
+                    <h3>44</h3>
+                    <p>Anggota Ideal</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-ios-checkmark-outline" />
+                  </div>
+                  <a href="#" class="small-box-footer">
+                    Detail Info <i class="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+              {/* ./col */}
+              <div class="col-lg-3 col-6">
+                {/* small box */}
+                <div class="small-box bg-secondary">
+                  <div class="inner">
                     <h3>65</h3>
                     <p>Anggota Izin Sakit</p>
                   </div>
                   <div class="icon">
-                    <i class="ion ion-pie-graph" />
+                    <i class="ion ion-ios-list" />
                   </div>
                   <a href="#" class="small-box-footer">
                     Detail Info <i class="fas fa-arrow-circle-right" />
